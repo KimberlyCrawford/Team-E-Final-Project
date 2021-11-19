413f843e5074d7f250a2f702898e47381c33d5f1
GaryG484 <85652494+GaryG484@users.noreply.github.com>
2021-11-18 20:50:40 -0600

    Map code should work now.

--- Static/js/Vacation_Climate_logic.js
+++ Static/js/Vacation_Climate_logic.js
@@ -1,44 +1,32 @@
+// Add console.log to check to see if our code is working.
+// Add console.log to check to see if our code is working.
+console.log("working");
 
-// Add the map tile layer
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// Create a base layer that holds both maps.
-let baseMaps = {
-    Street: streets,
-    Dark: dark
-  };
-
-  // Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [streets]
-})
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
+// We create the map object with options.
+let map = L.map('mapid').setView([60, 60], 1);
 
 // Get data from cities.js
 let cityData = cities;
 
-
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
-  console.log(city)
-  L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.avgtemp + "</h2> <hr> <h3>Cost per sq/m " + city.cost + "</h3>")
-.addTo(map);
+	console.log(city)
+	L.marker(city.location, {
+		city: city.population,
+		avgtemp: city.avgtemp,
+		cost: city.cost
+	})
+	.bindPopup("<h2>" + city.city + ", AvgTemp: " + city.avgtemp + "</h2> <hr> <h3>Cost USD: " + city.cost.toLocaleString() + "</h3>")
+  .addTo(map);
 });
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+  id: "mapbox/streets-v10",
+	accessToken: API_KEY
+});
+// Then we add our 'streets' tile layer to the map.
+streets.addTo(map);
